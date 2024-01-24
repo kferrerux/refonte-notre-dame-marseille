13942e15a04d758a9a4c6335e070430d4673066e
kferrerux <ferrerkevin@hotmail.fr>
2024-01-24 09:19:07 +0100

    various fix

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 
-// navbar
+// NAVBAR RESPONSIVE
 //-------------------------------------------
 
 // Initialisation de la variable scroll à valeur 0
@@ -9,13 +9,13 @@ let scroll = 0;
 window.addEventListener('scroll', () => {
 
 // Structure conditionnelle 
-// Si la valeur de scroll verticale(Y) est inférieure à la valeur de la variable scroll (0) 
+    // Si la valeur de scroll verticale(Y) est inférieure à la valeur de la variable scroll (0) 
     if (window.scrollY < scroll) {
         // Alors on applique ces styles CSS (top 0 / Opacitée 0.9)
         header.style.top = '0';   
         header.style.transition = 'all 0.5s ease';
         header.style.opacity = '0.9';
-// Si la valeur de scroll verticale(Y) est supérieure à la valeur de la variable scroll (0)  
+    // Si la valeur de scroll verticale(Y) est supérieure à la valeur de la variable scroll (0)  
     } if (window.scrollY > scroll) {
         // Alors on applique ces styles CSS (top -121px (la barre est invisible pour le navigateur) / Opacitée 0.9)
         header.style.top = '-121px';
@@ -28,7 +28,5 @@ window.addEventListener('scroll', () => {
     //  console.log(scroll);
 })
 
-// fin de la navbar
+// FIN DE LA NAVBAR RESPONSIVE
 //-------------------------------------------
-
-// 
