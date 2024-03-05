6d02551e23a3c36f730dc7a6194f1d099f8a89af
kferrerux <ferrerkevin@hotmail.fr>
2024-03-05 10:58:50 +0100

    add online support

--- database/db-connect.php
+++ database/db-connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=notredamedelagarde;charset=utf8', 'root', 'EdenManon.');
+    $db = new PDO('mysql:host=localhost;dbname=dbs12655734;charset=utf8', 'root', 'EdenManon.');
 } catch (Exception $e) {
     echo
 
